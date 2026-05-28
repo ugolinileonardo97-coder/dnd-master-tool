a6c8cdd35e21d19283834a4d6e871796af2e58e0
Leonardo Ugolini <ugolini.leonardo97@gmail.com>
2026-05-28 20:28:51 +0200

    Fix PWA cache immagini bestiario

--- vite.config.js
+++ vite.config.js
@@ -5,27 +5,37 @@ import { VitePWA } from "vite-plugin-pwa";
 export default defineConfig({
   plugins: [
     react(),
+
     VitePWA({
       registerType: "autoUpdate",
-      includeAssets: ["favicon.svg"],
+
+      includeAssets: ["favicon.svg", "icons.svg"],
+
+      workbox: {
+        maximumFileSizeToCacheInBytes: 5 * 1024 * 1024,
+      },
+
       manifest: {
-        name: "D&D Master Tool",
-        short_name: "D&D Tool",
-        description: "Assistente offline per shop e locande D&D",
-        theme_color: "#080604",
-        background_color: "#080604",
+        name: "D&D Shop Master",
+        short_name: "D&D Shop",
+        description: "Strumento fantasy per Dungeon Master",
+        start_url: "/",
+        scope: "/",
         display: "standalone",
-        orientation: "landscape",
+        background_color: "#070504",
+        theme_color: "#070504",
         icons: [
           {
-            src: "/vite.svg",
+            src: "/icons.svg",
             sizes: "192x192",
             type: "image/svg+xml",
           },
+          {
+            src: "/icons.svg",
+            sizes: "512x512",
+            type: "image/svg+xml",
+          },
         ],
-      },
-      workbox: {
-        globPatterns: ["**/*.{js,css,html,ico,png,svg}"],
       },
     }),
   ],
